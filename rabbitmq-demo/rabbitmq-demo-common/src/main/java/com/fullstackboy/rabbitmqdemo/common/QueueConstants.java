8daba0c2da20f39826240ea85ba8fa93e101e8e7
Liuyongfei <liuyongfei@yunzongnet.com>
2020-05-18 13:20:05 +0800

    first commit

---
+++ rabbitmq-demo/rabbitmq-demo-common/src/main/java/com/fullstackboy/rabbitmqdemo/common/QueueConstants.java
@@ -0,0 +1,14 @@
+package com.fullstackboy.rabbitmqdemo.common;
+
+/**
+ * 定义队列所需要的常量
+ * @公众号 全栈在路上
+ * @GitHub https://github.com/liuyongfei1
+ * @author lyf
+ * @date 2020-05-17 18:30
+ */
+public class QueueConstants {
+    public static final String QUEUE_NAME = "demo2_queue";
+    public static final String QUEUE_EXCHANGE_NAME = "demo2_exchange";
+    public static final String QUEUE_ROUTING_KEY_NAME = "demo2_routing_key";
+}
