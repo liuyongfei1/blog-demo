91951e6e5c69275236d9428c85d6b346aed57bab
Liuyongfei <liuyongfei198805@gmail.com>
2020-05-25 14:19:00 +0800

    Update ProducerController.java
    修改生产者代码

--- rabbitmq-demo/rabbitmq-demo-web/src/main/java/com/fullstackboy/rabbitmqdemo/controller/ProducerController.java
+++ rabbitmq-demo/rabbitmq-demo-web/src/main/java/com/fullstackboy/rabbitmqdemo/controller/ProducerController.java
@@ -8,6 +8,7 @@ import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.TimeoutException;
 
 /**
  * 消息生产端
@@ -70,5 +72,41 @@ public class ProducerController {
         mp.setContentEncoding("UTF-8");
         mp.setContentLength((long)s.length());
         return new Message(src, mp);
+    }
+
+
+    public String sendAndReceive(String request) throws TimeoutException {
+        log.info("请求报文：{}" , request);
+
+        //请求结果
+        String result = null;
+
+        //设置消息唯一id
+        CorrelationData correlationId = new CorrelationData(UUID.randomUUID().toString());
+
+        // 直接发送消息
+        MessageProperties messageProperties = new MessageProperties();
+        messageProperties.setExpiration("10000");
+        messageProperties.setCorrelationId(correlationId.getId());
+
+        Message message = new Message(request.getBytes(),messageProperties);
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start("direct模式下rpc请求耗时");
+        Message response = rabbitTemplate.sendAndReceive(QueueConstants.QUEUE_EXCHANGE_NAME,
+                QueueConstants.QUEUE_ROUTING_KEY_NAME,message,correlationId);
+
+        stopWatch.stop();
+        log.info(stopWatch.getLastTaskName() + ":" +  stopWatch.getTotalTimeMillis());
+        if (response != null) {
+            result = new String(response.getBody());
+            log.info("请求成功，返回的结果为：{}", result);
+        } else {
+            log.error("请求超时");
+
+            // 为了方便jmeter测试，这里抛出异常
+            throw new TimeoutException("请求超时");
+        }
+        return result;
     }
 }
