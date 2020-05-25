289c9c5f35c92c398c6bc2ae9ecac0f8a77a9603
Liuyongfei <liuyongfei198805@gmail.com>
2020-05-25 13:55:55 +0800

    在RabbitConfig里添加RPC配置
    在RabbitConfig里添加RPC配置

--- rabbitmq-demo/rabbitmq-demo-consumer/src/main/java/com/fullstackboy/rabbitmqdemoconsumer/controller/ConsumerController.java
+++ rabbitmq-demo/rabbitmq-demo-consumer/src/main/java/com/fullstackboy/rabbitmqdemoconsumer/controller/ConsumerController.java
@@ -2,37 +2,57 @@ package com.fullstackboy.rabbitmqdemoconsumer.controller;
 
 import com.fullstackboy.rabbitmqdemo.common.QueueConstants;
 import com.rabbitmq.client.Channel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.AmqpHeaders;
-import org.springframework.messaging.Message;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 /**
  * 消息消费端（RPC服务端）
+ *
+ * @author Liuyongfei
  * @公众号 全栈在路上
  * @GitHub https://github.com/liuyongfei1
- * @author Liuyongfei
  * @date 2020-05-24 10:30
  */
+@Slf4j
 @Component
 public class ConsumerController {
 
-    @RabbitListener(queues = {QueueConstants.QUEUE_NAME})
-    public void handler(Message message, Channel channel) throws IOException {
-        System.out.println("收到消息：" + message.toString());
-        MessageHeaders headers = message.getHeaders();
-        Long tag = (Long) headers.get(AmqpHeaders.DELIVERY_TAG);
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
 
-        try {
-            // 手动确认消息已消费
-            channel.basicAck(tag,false);
-        } catch (IOException e) {
-            // 把消费失败的消息重新放入到队列，以后可以继续消费该条消息
-            channel.basicNack(tag, false, true);
-            e.printStackTrace();
-        }
+    @RabbitListener(queues = {QueueConstants.TOPIC_QUEUE1})
+    public void handler(Message msg, Channel channel) throws IOException {
+        log.info("队列1:" + msg.toString());
+        String msgBody = new String(msg.getBody());
+        //数据处理，返回的Message
+        Message repMsg = con(msgBody + "返回了", msg.getMessageProperties().getCorrelationId());
+
+        rabbitTemplate.send(QueueConstants.TOPIC_EXCHANGE, QueueConstants.TOPIC_QUEUE2, repMsg);
+    }
+
+    @RabbitListener(queues=QueueConstants.TOPIC_QUEUE2)
+    public void receiveTopic2(Message msg) {
+        log.info("队列2:"+msg.toString());
+
+    }
+
+    public Message con(String s, String id) {
+        MessageProperties mp = new MessageProperties();
+        byte[] src = s.getBytes(Charset.forName("UTF-8"));
+        mp.setContentType("application/json");
+        mp.setContentEncoding("UTF-8");
+        mp.setCorrelationId(id);
+
+        return new Message(src, mp);
     }
 }
