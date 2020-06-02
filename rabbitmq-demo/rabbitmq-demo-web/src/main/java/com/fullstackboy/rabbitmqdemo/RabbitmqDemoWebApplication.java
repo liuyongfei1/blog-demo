c06a63fb2b406f329f7edc67a2fce5c4af16ca85
liuyongfei1 <liuyongfei198805@gmail.com>
2020-06-02 10:39:38 +0800

    添加数据库连接配置
    添加数据库连接配置

--- rabbitmq-demo/rabbitmq-demo-web/src/main/java/com/fullstackboy/rabbitmqdemo/RabbitmqDemoWebApplication.java
+++ rabbitmq-demo/rabbitmq-demo-web/src/main/java/com/fullstackboy/rabbitmqdemo/RabbitmqDemoWebApplication.java
@@ -1,9 +1,11 @@
 package com.fullstackboy.rabbitmqdemo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.fullstackboy.rabbitmqdemo.mapper")
 public class RabbitmqDemoWebApplication {
 
     public static void main(String[] args) {
