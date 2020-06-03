0605f77c1b4083ed0ce80f19f1ed6c389cce9a6e
liuyongfei1 <liuyongfei198805@gmail.com>
2020-06-03 11:02:18 +0800

    模拟高并发抢单v1

--- rabbitmq-demo/rabbitmq-demo-service/src/main/java/com/fullstackboy/rabbitmqdemo/service/CommonMqService.java
+++ rabbitmq-demo/rabbitmq-demo-service/src/main/java/com/fullstackboy/rabbitmqdemo/service/CommonMqService.java
@@ -3,9 +3,12 @@ package com.fullstackboy.rabbitmqdemo.service;
 import com.fullstackboy.rabbitmqdemo.common.QueueConstants;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 /**
  * 发送抢单信息进入队列
@@ -22,9 +25,11 @@ public class CommonMqService {
 
     public void sendRobbingMsg(String mobile) {
         String message = mobile;
+        // 生成消息的唯一id
+        String msgId = UUID.randomUUID().toString();
         try {
             rabbitTemplate.convertAndSend(QueueConstants.QUEUE_EXCHANGE_NAME,QueueConstants.QUEUE_ROUTING_KEY_NAME,
-                    message);
+                    message, new CorrelationData(msgId));
         } catch (AmqpException e) {
             log.error("发送抢单信息进入队列时发生异常，mobile: [{}]",mobile);
         }
