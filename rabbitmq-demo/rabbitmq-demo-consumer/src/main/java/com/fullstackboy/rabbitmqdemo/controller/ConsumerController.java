c862ab002cc734494621df4acc135e853b481995
liuyongfei1 <liuyongfei198805@gmail.com>
2020-06-10 17:36:56 +0800

    确保每个消费者在同一个时间点最多只处理一个Message
    确保每个消费者在同一个时间点最多只处理一个Message

--- rabbitmq-demo/rabbitmq-demo-consumer/src/main/java/com/fullstackboy/rabbitmqdemo/controller/ConsumerController.java
+++ rabbitmq-demo/rabbitmq-demo-consumer/src/main/java/com/fullstackboy/rabbitmqdemo/controller/ConsumerController.java
@@ -30,7 +30,8 @@ public class ConsumerController {
 
     @RabbitListener(queues = {QueueConstants.QUEUE_NAME}, containerFactory = "customContainerFactory")
     public void handler(Message message, Channel channel) throws IOException {
-
+        // 确保每个消费者在同一个时间点最多只处理一个Message，换句话说，在接收到该消费者的ack之前，rabbitmq server不会将新的Message分发给该消费者。
+        channel.basicQos(1);
         // 获取手机号
         String mobile = (String) message.getPayload();
         log.info("监听到抢单手机号：[{}]", mobile);
@@ -43,8 +44,12 @@ public class ConsumerController {
             // 处理抢单逻辑
             concurrencyService.manageRobbing(mobile);
 
+            // 模拟耗时任务
+            Thread.sleep(1000 * 60);
+
             // 确认消息已经消费
             channel.basicAck(tag, false);
+            log.info("用户 : [{}] 商城抢单成功", mobile);
 
             // 异步通知用户抢单成功
         } catch (Exception e) {
