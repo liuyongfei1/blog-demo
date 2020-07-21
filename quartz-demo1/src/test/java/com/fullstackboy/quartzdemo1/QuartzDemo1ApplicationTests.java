64be4347b287c104e702b65b5b1d1aa4d9535af2
liuyongfei1 <liuyongfei198805@gmail.com>
2020-07-21 13:14:10 +0800

    添加quartz-demo模块
    添加quartz-demo模块

---
+++ quartz-demo1/src/test/java/com/fullstackboy/quartzdemo1/QuartzDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstackboy.quartzdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzDemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
