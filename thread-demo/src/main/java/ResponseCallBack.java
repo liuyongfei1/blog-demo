fc3e6195a8bd96e8f564135e38ce50c3cd9657d8
liuyongfei1 <liuyongfei198805@gmail.com>
2021-02-26 11:02:40 +0800

    添加：thread-demo

---
+++ thread-demo/src/main/java/ResponseCallBack.java
@@ -0,0 +1,14 @@
+/**
+ * 响应消息的接口类
+ * @author Liuyongfei
+ * @date 2021/2/26 上午10:58
+ */
+public interface ResponseCallBack {
+    /**
+     * 打印消息
+     * @param msg 要打印的消息
+     * @author Liuyongfei
+     * @date 2021/2/26 上午10:58
+     */
+    public void printMsg(String msg);
+}
---
+++ thread-demo/src/main/java/Test1.java
@@ -0,0 +1,21 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 模拟耗时任务异步执行
+ * 耗时任务开启单独线程处理，任务线程处理完毕通知主线程
+ * @author Liuyongfei
+ * @date 2020/6/10 14:56
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        executorService.submit(new TestThread(new ResponseCallBack() {
+            @Override
+            public void printMsg(String msg) {
+                System.out.println("print message: " + msg);
+            }
+        }));
+    }
+}
+
