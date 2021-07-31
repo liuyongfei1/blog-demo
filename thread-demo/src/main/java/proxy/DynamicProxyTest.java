fe9ef3ccf981a612fcbb8781bf7a1908b6e1989e
liuyongfei <liuyongfei198805@gmail.com>
2021-07-31 16:52:42 +0800

    添加动态代理demo
    添加动态代理demo

---
+++ thread-demo/src/main/java/proxy/DynamicProxyTest.java
@@ -0,0 +1,16 @@
+package proxy;
+
+/**
+ * 动态代理测试
+ *
+ * @author Liuyongfei
+ * @date 2021/7/31 10:40
+ */
+public class DynamicProxyTest {
+
+    public static void main(String[] args) {
+        IPerson person = (IPerson) DynamicProxyFactory.getProxy(new IPersonImpl());
+        person.sleep();
+        person.eating();
+    }
+}
---
+++ thread-demo/src/main/java/proxy/IPerson.java
@@ -0,0 +1,13 @@
+package proxy;
+
+/**
+ * 定义接口
+ * @date 2021/07/31
+ */
+public interface IPerson {
+
+         public abstract void sleep();
+
+         public abstract void eating();
+
+}
