ce9a2e3d62680ae1aa079e314176360a39665b57
liuyongfei1 <liuyongfei198805@gmail.com>
2021-08-15 19:04:38 +0800

    JDK并发包源码剖析：微服务注册中心的服务注册案例-优化代码
    JDK并发包源码剖析：微服务注册中心的服务注册案例-优化代码，将Worker类做为Client类的内部类

--- regisster-client-demo/src/main/java/com/fullstackboy/register/client/RegisterClient.java
+++ regisster-client-demo/src/main/java/com/fullstackboy/register/client/RegisterClient.java
@@ -28,4 +28,84 @@ public class RegisterClient {
     public void start() {
         new RegisterClientWorker(serviceInstanceId).start();
     }
+
+    /**
+     * 负责向register-server发起服务注册和心跳的线程
+     * 注释：
+     * RegisterClient是一个核心组件
+     * RegisterClientWorker是完成RegisterClient类的核心功能，是属于RegisterClient的一个组件，在面向对象层面来说，比较常见的写法是做为一个内部类来使用，
+     * 在开源项目中经常采取这种做法。
+     * @author Liuyongfei
+     * @date 2021/8/15 17:54
+     */
+    private class RegisterClientWorker extends Thread{
+
+        private static final String SERVICE_NAME = "inventory-service";
+        private static final String IP = "192.168.10.111";
+        private static final String HOST_NAME = "inventory01";
+        private static final String PORT = "9000";
+
+        /**
+         * http通信组件
+         */
+        private HttpSender httpSender;
+
+        /**
+         * 是否完成注册
+         */
+        private Boolean finishedRegister;
+
+        /**
+         * 服务实例id
+         */
+        private String serverInstanceId;
+
+        public RegisterClientWorker( String serverInstanceId) {
+            this.httpSender = new HttpSender();
+            this.finishedRegister = false;
+            this.serverInstanceId = serverInstanceId;
+        }
+
+        @Override
+        public void run() {
+            // 如果还没有完成注册
+            if (!finishedRegister) {
+                // 可以从配置文件里获取到当前机器的hostname，ip，以及你的服务的端口号
+                RegisterRequest request = new RegisterRequest();
+                request.setServiceName(SERVICE_NAME);
+                request.setHostName(HOST_NAME);
+                request.setIp(IP);
+                request.setPort(PORT);
+                request.setServerInstanceId(serverInstanceId);
+                RegisterResponse response = httpSender.register(request);
+                System.out.println("服务注册的结果是：【" + response.getStatus() + "】......");
+
+                // 如果注册成功
+                if (RegisterResponse.SUCCESS.equals(response.getStatus())) {
+                    finishedRegister = true;
+                } else {
+                    return;
+                }
+            }
+
+            // 如果说完成注册，则每隔30秒向注册中心发送心跳
+            if (finishedRegister) {
+                HeartbeatRequest request = new HeartbeatRequest();
+                request.setServerInstanceId(serverInstanceId);
+
+                HeartbeatResponse response = null;
+                while (true) {
+                    try {
+                        response = httpSender.heartbeat(request);
+                        System.out.println("心跳的结果为：【" + response.getStatus() + "】......");
+                        Thread.sleep(30 * 1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+
+            }
+        }
+    }
 }
