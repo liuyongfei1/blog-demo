b58ef0eb9e2e6d86c98ab507a3c891c2dd0ebf4e
liuyongfei1 <liuyongfei198805@gmail.com>
2021-08-16 22:43:37 +0800

    添加微服务注册中心案例
    添加微服务注册中心案例

---
+++ register-server-demo/src/main/java/com/fullstackboy/register/server/Registry.java
@@ -0,0 +1,55 @@
+package com.fullstackboy.register.server;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 注册表
+ *
+ * @author Liuyongfei
+ * @date 2021/8/16 22:10
+ */
+public class Registry {
+
+    /**
+     * 注册表
+     */
+    private static final Map<String, Map<String, ServiceInstance>> registry = new HashMap<>();
+
+    /**
+     * 服务注册
+     */
+    public void register(ServiceInstance instance) {
+        Map<String, ServiceInstance> instanceMap = registry.get(instance.getServiceName());
+        if (instanceMap == null) {
+            instanceMap = new HashMap<>();
+            registry.put(instance.getServiceName(), instanceMap);
+        }
+
+        instanceMap.put(instance.getServiceInstanceId(),instance);
+
+        // ???
+//        ServiceInstance instance = new ServiceInstance();
+//        instance.setHostName(registerRequest.getHostName());
+//        instance.setIp(registerRequest.getIp());
+//        instance.setServiceName(registerRequest.getServiceName());
+//        // Lease???
+//        instance.setLease();
+//        instanceMap.put(registerRequest.getServerInstanceId(),instance);
+//
+//        registry.put(registerRequest.getServiceName(), instanceMap);
+    }
+
+    /**
+     * 服务摘除
+     * @param serviceName 服务名称
+     * @param serverInstanceId 服务实例id
+     * @author Liuyongfei
+     * @date 2021/8/16 下午10:40
+     */
+    public void remove(String serviceName, String serverInstanceId) {
+        // 获取注册表
+        Map<String,ServiceInstance> serverInstanceMap = registry.get(serviceName);
+        serverInstanceMap.remove(serverInstanceId);
+    }
+}
