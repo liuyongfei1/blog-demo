8afe596f709bf464d8e3304318c1e03b6b98a280
liuyongfei1 <liuyongfei198805@gmail.com>
2021-08-21 12:27:59 +0800

    添加Thread的wait和notify案例
    添加Thread的wait和notify案例

--- thread-demo/src/main/java/concurrent/QueueWaitAndNotifyDemo.java
+++ thread-demo/src/main/java/concurrent/QueueWaitAndNotifyDemo.java
@@ -33,6 +33,7 @@ public class QueueWaitAndNotifyDemo {
             }
             // 队列没有满，则向队列中放入数据
             queue.add(element);
+            System.out.println("往队列中添加了元素：【" + element + "】");
             notifyAll(); // 唤醒当前在等待这个锁的那些线程
         } catch (InterruptedException e) {
             e.printStackTrace();
@@ -51,6 +52,7 @@ public class QueueWaitAndNotifyDemo {
                 wait();// 释放掉这把锁，进入阻塞，这时其它线程加锁成功，往队列中放入数据
             }
             element = queue.removeFirst();
+            System.out.println("从队列中取出了元素：【" + element + "】");
             notifyAll(); // 唤醒当前在等待这个锁的那些线程
         } catch (InterruptedException e) {
             e.printStackTrace();
@@ -58,7 +60,28 @@ public class QueueWaitAndNotifyDemo {
         return element;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+        QueueWaitAndNotifyDemo queue  = new QueueWaitAndNotifyDemo();
+
+        new Thread() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 11; i++) {
+                    queue.offer("元素_" + i);
+                }
+            }
+        }.start();
+
+        Thread.sleep(1000);
+
+        new Thread() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 11; i++) {
+                    queue.take();
+                }
+            }
+        }.start();
 
     }
 }
