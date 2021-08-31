570a391b912847aa10a885429dc6d166a65e91aa
liuyongfei1 <liuyongfei198805@gmail.com>
2021-08-31 19:54:55 +0800

    添加CountDownLatchDemo
    添加CountDownLatchDemo

--- register-server-demo/src/main/java/com/fullstackboy/register/server/ServiceAliveMonitor.java
+++ register-server-demo/src/main/java/com/fullstackboy/register/server/ServiceAliveMonitor.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 /**
  * 服务实例存活监控线程
- * 
+ *
  * 优化：加入自我保护机制
  *
  * @author Liuyongfei
---
+++ thread-demo/src/main/java/concurrent/CountDownLatchDemo.java
@@ -0,0 +1,68 @@
+package concurrent;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * 一个线程启动了多个线程来执行任务，且该线程需要阻塞等待其它的线程执行完毕后才能继续向下走
+ *
+ * 这时就可以用CountDownLatch。
+ * @author Liuyongfei
+ * @date 2021/8/31 19:41
+ */
+public class CountDownLatchDemo {
+    static CountDownLatch cdt = new CountDownLatch(3);
+
+    public static void main(String[] args){
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2 * 1000);
+                    System.out.println("线程一执行完毕");
+                    CountDownLatchDemo.cdt.countDown();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2 * 1000);
+                    System.out.println("线程二执行完毕");
+                    CountDownLatchDemo.cdt.countDown();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2 * 1000);
+                    System.out.println("线程三执行完毕");
+                    CountDownLatchDemo.cdt.countDown();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    System.out.println("main线程被阻塞");
+                    CountDownLatchDemo.cdt.await();
+                    System.out.println("main线程被唤醒，可继续向下执行");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+}
