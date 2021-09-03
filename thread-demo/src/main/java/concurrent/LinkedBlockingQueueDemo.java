49cb4aaa9164c5663a274d0f1b3e89618161d4d3
liuyongfei1 <liuyongfei198805@gmail.com>
2021-09-03 23:53:08 +0800

    Update LinkedBlockingQueueDemo.java
    修改

--- thread-demo/src/main/java/concurrent/LinkedBlockingQueueDemo.java
+++ thread-demo/src/main/java/concurrent/LinkedBlockingQueueDemo.java
@@ -1,5 +1,6 @@
 package concurrent;
 
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 /**
@@ -10,8 +11,15 @@ import java.util.concurrent.LinkedBlockingQueue;
  */
 public class LinkedBlockingQueueDemo {
     public static void main(String[] args) throws Exception{
-        LinkedBlockingQueue<String> queue = new LinkedBlockingQueue<>();
-        queue.put("张三");
-        System.out.println(queue.take());
+        LinkedBlockingQueue<String> queue = new LinkedBlockingQueue<>(1);
+        queue.offer("张三");
+        queue.offer("李四");
+//        System.out.println(queue.take());
+
+        ArrayBlockingQueue queue1 = new ArrayBlockingQueue(2);
+        queue1.add("张三");
+        queue1.add("赵四");
+
+        System.out.println(queue1);
     }
 }
