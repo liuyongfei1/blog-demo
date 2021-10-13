4f3756cf7413941e4cb7b5d0f5e4b719a0416deb
liuyongfei1 <liuyongfei198805@gmail.com>
2021-10-13 23:37:52 +0800

    添加KafkaProducerDemo
    添加KafkaProducerDemo

---
+++ test-demo/src/main/java/com/fullstackboy/KafkaConsumerDemo.java
@@ -0,0 +1,10 @@
+package com.fullstackboy;
+
+/**
+ * Kafka消费端
+ *
+ * @author Liuyongfei
+ * @date 2021/10/12 16:24
+ */
+public class KafkaConsumerDemo {
+}
---
+++ thread-demo/src/main/java/synchronizeddemo/Demo1.java
@@ -0,0 +1,13 @@
+package synchronizeddemo;
+
+/**
+ * TODO Liuyongfei
+ *
+ * @author Liuyongfei
+ * @date 2021/10/11 20:43
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+
+    }
+}
