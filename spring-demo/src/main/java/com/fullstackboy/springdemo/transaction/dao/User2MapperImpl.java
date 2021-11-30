1d11d727163057403c6f80fa286fa71328cbbfb3
liuyongfei1 <liuyongfei198805@gmail.com>
2021-11-30 13:37:40 +0800

    Spring整合Mybatis：使用SqlSessionDaoSupport
    Spring整合Mybatis：使用SqlSessionDaoSupport

---
+++ spring-demo/src/main/java/com/fullstackboy/springdemo/transaction/dao/User2MapperImpl.java
@@ -0,0 +1,24 @@
+package com.fullstackboy.springdemo.transaction.dao;
+
+import com.fullstackboy.springdemo.transaction.pojo.User;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+
+import java.util.List;
+
+/**
+ * 整合Mybatis方式二
+ *
+ * @author Liuyongfei
+ * @date 2021/11/30 13:21
+ */
+public class User2MapperImpl extends SqlSessionDaoSupport implements UserMapper{
+    @Override
+    public List<User> getUserList() {
+        return getSqlSession().getMapper(UserMapper.class).getUserList();
+    }
+
+    @Override
+    public User getUserById(int id) {
+        return getSqlSession().getMapper(UserMapper.class).getUserById(id);
+    }
+}
---
+++ spring-demo/src/test/java/com/fullstackboy/springdemo/transaction/dao/TestUser2Mapper.java
@@ -0,0 +1,33 @@
+package com.fullstackboy.springdemo.transaction.dao;
+
+import com.fullstackboy.springdemo.transaction.pojo.User;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * 单元测试类
+ * 使用第二种整合方式： 使用SqlSessionDaoSupport
+ * @author Liuyongfei
+ * @date 2021/11/29 23:49
+ */
+
+public class TestUser2Mapper {
+
+    @Test
+    public void getUserListTest() {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        UserMapper mapper = applicationContext.getBean("userMapper2",UserMapper.class);
+
+        for (User user : mapper.getUserList()) {
+            System.out.println(user);
+        }
+
+       /* 输出结果
+        User(id=1, name=张三, pwd=123)
+        User(id=2, name=李四, pwd=567)
+        User(id=3, name=王五, pwd=789)*/
+
+    }
+}
