fc70bf9350bc73f18c644c6b31e030c6ee83acdf
liuyongfei1 <liuyongfei198805@gmail.com>
2021-12-02 21:55:47 +0800

    第一个Spring MVC程序
    第一个Spring MVC程序

--- springmvc-01-demo/src/main/java/com/fullstackboy/springmvcdemo/controller/RegisterController.java
+++ springmvc-01-demo/src/main/java/com/fullstackboy/springmvcdemo/controller/RegisterController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
+ * 第一个Spring MVC 程序
  * 注册控制器
  * Controller是控制器接口，接口中只有一个方法handleRequest，用户处理请求和返回ModelAndView
  * @author Liuyongfei
