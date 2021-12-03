fc82858ca548c46aec21e49aa47501bc07ffbf89
liuyongfei1 <liuyongfei198805@gmail.com>
2021-12-03 09:41:36 +0800

    Spring MVC：@RestController注解和@ResponseBody注解
    Spring MVC：@RestController注解和@ResponseBody注解不会走视图解析器，会直接返回结果

--- springmvc-02-demo/src/main/java/com/fullstackboy/springmvcdemo/controller/UserController.java
+++ springmvc-02-demo/src/main/java/com/fullstackboy/springmvcdemo/controller/UserController.java
@@ -1,11 +1,17 @@
 package com.fullstackboy.springmvcdemo.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fullstackboy.springmvcdemo.pojo.User;
 import com.fullstackboy.springmvcdemo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * 用户控制器
@@ -29,14 +35,21 @@ public class UserController {
      * @return
      */
     @RequestMapping("/login")
+//    @ResponseBody  // 加上该注解后，controller收到该请求，就不会走视图解析器，而是会直接返回一个字符串。
     public String login(Model model) {
         User user = new User();
         user.setName("bianchengbang");
 
         userService.login(user);
 
+
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        Date date = new Date();
+//        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd H:i:s");
+//        dateFormat.format(date);
+
         model.addAttribute("user",user);
-        return "login";
+        return "login1";
     }
 
     /**
--- springmvc-02-demo/src/main/java/com/fullstackboy/springmvcdemo/pojo/User.java
+++ springmvc-02-demo/src/main/java/com/fullstackboy/springmvcdemo/pojo/User.java
@@ -1,6 +1,8 @@
 package com.fullstackboy.springmvcdemo.pojo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * 用户实体类
@@ -9,6 +11,8 @@ import lombok.Data;
  * @date 2021/12/2 22:04
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     private String name;
