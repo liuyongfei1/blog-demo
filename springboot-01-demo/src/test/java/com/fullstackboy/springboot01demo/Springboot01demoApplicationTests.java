3c75409b40baa525958db4ead57f35a8d0bdb03d
liuyongfei1 <liuyongfei198805@gmail.com>
2021-12-09 13:18:10 +0800

    Spring Boot Web开发-导入静态资源
    Spring Boot Web开发-导入静态资源

---
+++ springboot-01-demo/src/test/java/com/fullstackboy/springboot01demo/Springboot01demoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstackboy.springboot01demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot01demoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
