f6db5faf9ce69e28e2d1f6bec971f83f4111a5f2
liuyongfei1 <liuyongfei198805@gmail.com>
2021-12-28 13:09:55 +0800

    Java基础：字符拓展
    Java基础：字符拓展

--- test-demo/src/main/java/com/fullstackboy/test/Demo1.java
+++ test-demo/src/main/java/com/fullstackboy/test/Demo1.java
@@ -8,6 +8,20 @@ package com.fullstackboy.test;
  */
 public class Demo1 {
     public static void main(String[] args) {
+        // 整数型拓展
+
+        int i =  10;
+        // 八进制以0开头
+        int i2 = 010;
+
+        // 十六进制以0x开头  0~9 + A-F
+        int i3 = 0x10;
+        System.out.println(i);
+        System.out.println(i2);
+        System.out.println(i3);
+        System.out.println("------------");
+
+        // 浮点数拓展
         float d1 = 12313123423423423423f;
         float d2 = d1 + 1;
 
@@ -20,5 +34,28 @@ public class Demo1 {
 
         // 那银行业务时，使用BigDecimal，是一个数学工具类。
         System.out.println(d1 == d2);
+
+        System.out.println("------------");
+        // 字符拓展
+        char c1 = 'a';
+        char c2 = '中';
+        System.out.println(c1);
+
+        // 强制转换，所有的字符本质还是数字  char会涉及到一个编码问题，Unicode可以处理各种编码，占2个字节  0~ 65536
+        // 编码 Unicode表: （97 = a,  65 = A）
+
+        // 经常会看到使用Unicode编码使用转义来表示  U0000 UFFFF
+         char c3 = '\u0061'; // 这是正常的unicode编码
+
+        System.out.println((int)c1);  // 强制转换   97
+        System.out.println(c2);
+        System.out.println((int)c2); // 强制转换   20013
+        System.out.println(c3);  // a
+
+        // 转义字符
+        // \t 制表符
+        // \n 换行
+        // ......
+        System.out.println("Hello\nWorld"); // 会输出换行的 Hell World
     }
 }
