599f370b70117dbb058bb968716b59e09fd0d7f0
liuyongfei1 <liuyongfei198805@gmail.com>
2022-03-11 17:14:33 +0800

    添加使用DOM 读取xml 文件信息
    添加使用DOM 读取xml 文件信息

---
+++ test-demo/src/main/java/com/fullstackboy/dmoparsexml/DomUtil.java
@@ -0,0 +1,70 @@
+package com.fullstackboy.dmoparsexml;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 使用DOM解析XML工具类
+ *
+ * @author Liuyongfei
+ * @date 2022/3/11 16:44
+ */
+public class DomUtil {
+
+    /**
+     * 将指定路径下的XML解析成Document对象
+     * 这个Document对象包含了Student的所有信息
+     * @param xmlPath xml文件绝对路径
+     * @return Document对象
+     * @throws Exception
+     */
+    public static Document getDocument(String xmlPath) throws Exception {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = factory.newDocumentBuilder();
+
+            return documentBuilder.parse(xmlPath);
+        } catch (Exception e) {
+            throw new RuntimeException("解析XML文件失败!");
+        }
+    }
+
+    /**
+     * 从xml文件里获取到Student信息
+     * @param xmlPath xml文件绝对路径
+     * @return Student信息 list
+     * @throws Exception
+     */
+    public static List<Student> getStudents(String xmlPath) throws Exception {
+        // 1. 获取student.xml对应的Document对象
+        Document document = getDocument(xmlPath);
+
+        // 2. 获取XML中所有student的节点
+        List<Student> students = new ArrayList<>();
+        NodeList studentNodes = document.getElementsByTagName("students");
+
+        // 遍历每个 student 节点
+        for (int i = 0; i < studentNodes.getLength(); i++) {
+            Element studentEle = (Element) studentNodes.item(i);
+
+            Student student = new Student();
+            student.setName(studentEle.getElementsByTagName("name").item(0).getTextContent());
+            student.setAge(Integer.parseInt(studentEle.getElementsByTagName("age").item(0).getTextContent()));
+            students.add(student);
+        }
+        return students;
+    }
+
+    public static void main(String[] args) throws Exception {
+        // 使用DOM 读取xml 文件信息
+        String xmlPath = "/Users/lyf/Workspace/www/blog-demo/test-demo/src/main/resources/student.xml";
+        List<Student> students = getStudents(xmlPath);
+        System.out.println(students);
+    }
+}
---
+++ test-demo/src/main/java/com/fullstackboy/dmoparsexml/Student.java
@@ -0,0 +1,38 @@
+package com.fullstackboy.dmoparsexml;
+
+/**
+ * TODO Liuyongfei
+ *
+ * @author Liuyongfei
+ * @date 2022/3/11 16:41
+ */
+public class Student {
+
+    private String name;
+
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
